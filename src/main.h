fdeb3d9326308312b13d5e63bebab42e3899b93a
halosghost <halosghost@archlinux.info>
2017-01-22 11:21:29 -0600

    add main.h (missing from last commit)

---
+++ src/main.h
@@ -0,0 +1,14 @@
+#if !defined(PANDABIN_MAIN_H)
+#define PANDABIN_MAIN_H
+
+#pragma once
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <string.h>
+
+#define PROGNM "pandabin"
+
+#endif
