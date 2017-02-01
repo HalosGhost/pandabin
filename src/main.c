084a3b2c85a784e0d204e76f69f689e394b7aff1
halosghost <halosghost@archlinux.info>
2017-02-01 09:50:54 -0600

    separate directory initialization into its own function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+static signed
+dir_init (void);
+
 signed
 main (void) {
 
@@ -7,66 +10,74 @@ main (void) {
     openlog(PROGNM, LOG_CONS, LOG_USER);
     syslog(LOG_INFO, "Started\n");
 
-    { // directory initialization
-        char cwd [PATH_MAX] = "";
-        errno = 0;
-        if ( !getcwd(cwd, PATH_MAX - 1) ) {
-            status = errno;
-            syslog(LOG_ERR, "Could not store working directory: %s\n", strerror(status));
-            goto cleanup;
-        }
-
-        errno = 0;
-        if ( chdir(PREFIX) == -1 ) {
-            status = errno;
-            syslog(LOG_ERR, "Could not cd to %s: %s\n", PREFIX, strerror(status));
-            goto cleanup;
-        }
-
-        errno = 0;
-        if ( chdir(MAINPATH) == -1 ) {
-            syslog(LOG_INFO, "Could not cd to %s: %s\n", MAINPATH, strerror(errno));
-
-            errno = 0;
-            if ( mkdir(MAINPATH, 0777) == -1 ) {
-                status = errno;
-                syslog(LOG_ERR, "Could not create %s: %s\n", MAINPATH, strerror(status));
-                goto cleanup;
-            }
-
-            syslog(LOG_INFO, "Created %s\n", MAINPATH);
-            errno = 0;
-            if ( chdir(MAINPATH) == -1 ) {
-                status = errno;
-                syslog(LOG_ERR, "Still Could not cd to %s: %s\n", MAINPATH, strerror(status));
-                goto cleanup;
-            }
-        }
-
-        errno = 0;
-        if ( chdir(FILEPATH) == -1 ) {
-            syslog(LOG_INFO, "Could not cd to %s: %s\n", FILEPATH, strerror(errno));
-
-            errno = 0;
-            if ( mkdir(FILEPATH, 0777) == -1 ) {
-                status = errno;
-                syslog(LOG_ERR, "Could not create %s: %s\n", FILEPATH, strerror(status));
-                goto cleanup;
-            }
-
-            syslog(LOG_INFO, "Created %s\n", FILEPATH);
-        }
-
-        errno = 0;
-        if ( chdir(cwd) == -1 ) {
-            status = errno;
-            syslog(LOG_ERR, "Could not cd to %s: %s\n", cwd, strerror(status));
-            goto cleanup;
-        }
-    }
+    status = dir_init();
+    if ( status != EXIT_SUCCESS ) { goto cleanup; }
 
     cleanup:
         syslog(LOG_INFO, "Ended\n");
         closelog();
         return status;
+}
+
+static signed
+dir_init (void) {
+
+    signed status = EXIT_SUCCESS;
+
+    char cwd [PATH_MAX] = "";
+    errno = 0;
+    if ( !getcwd(cwd, PATH_MAX - 1) ) {
+        status = errno;
+        syslog(LOG_ERR, "Could not store working directory: %s\n", strerror(status));
+        goto cleanup;
+    }
+
+    errno = 0;
+    if ( chdir(PREFIX) == -1 ) {
+        status = errno;
+        syslog(LOG_ERR, "Could not cd to %s: %s\n", PREFIX, strerror(status));
+        goto cleanup;
+    }
+
+    errno = 0;
+    if ( chdir(MAINPATH) == -1 ) {
+        syslog(LOG_INFO, "Could not cd to %s: %s\n", MAINPATH, strerror(errno));
+
+        errno = 0;
+        if ( mkdir(MAINPATH, 0777) == -1 ) {
+            status = errno;
+            syslog(LOG_ERR, "Could not create %s: %s\n", MAINPATH, strerror(status));
+            goto cleanup;
+        }
+
+        syslog(LOG_INFO, "Created %s\n", MAINPATH);
+        errno = 0;
+        if ( chdir(MAINPATH) == -1 ) {
+            status = errno;
+            syslog(LOG_ERR, "Still Could not cd to %s: %s\n", MAINPATH, strerror(status));
+            goto cleanup;
+        }
+    }
+
+    errno = 0;
+    if ( chdir(FILEPATH) == -1 ) {
+        syslog(LOG_INFO, "Could not cd to %s: %s\n", FILEPATH, strerror(errno));
+
+        errno = 0;
+        if ( mkdir(FILEPATH, 0777) == -1 ) {
+            status = errno;
+            syslog(LOG_ERR, "Could not create %s: %s\n", FILEPATH, strerror(status));
+            goto cleanup;
+        }
+
+        syslog(LOG_INFO, "Created %s\n", FILEPATH);
+    }
+
+    cleanup:
+
+        errno = 0;
+        if ( chdir(cwd) == -1 ) {
+            status = errno;
+            syslog(LOG_ERR, "Could not cd to %s: %s\n", cwd, strerror(status));
+        } return status;
 }
