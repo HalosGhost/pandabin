b1d4f810a0fbd7f5eeeec1fed8e5d03a477afb99
halosghost <halosghost@archlinux.info>
2017-02-05 09:48:24 -0600

    Preliminary load-balancing across subdirectories

--- src/paste.c
+++ src/paste.c
@@ -24,16 +24,49 @@ pandabin_paste_new (const char * content, size_t size) {
     uuid_generate(pst->uuid);
     pst->size = size;
 
-    size_t pathlen = strlen(FILEPATH) + 39;
-    pst->path = malloc(pathlen);
+    size_t pathlen = strlen(FILEPATH) + 5;
+    pst->path = malloc(pathlen + 37);
     if ( !pst->path ) {
-        FAIL("%s: %s\n", "Failed to allocate path", strerror(status));
+        FAIL("Failed to allocate path: %s\n", strerror(status));
     }
 
-    signed s = snprintf(pst->path, pathlen, "%s/%s", FILEPATH, pst->hash);
+    signed s = snprintf(pst->path, pathlen, "%s/%.3s", FILEPATH, pst->hash);
     if ( s < 0 ) {
         errno = EXIT_FAILURE;
-        FAIL("%s\n", "Failed to store file path");
+        FAIL("Failed to store path to subdirectory\n");
+    }
+
+    char cwd [PATH_MAX] = "";
+    errno = 0;
+    if ( !getcwd(cwd, PATH_MAX - 1) ) {
+        FAIL("Failed to store working directory: %s\n", strerror(status));
+    }
+
+    errno = 0;
+    if ( chdir(pst->path) == -1 ) {
+        syslog(LOG_INFO, "Failed to cd to %s: %s\n", pst->path, strerror(errno));
+
+        errno = 0;
+        if ( mkdir(pst->path, 0777) == -1 ) {
+            FAIL("Failed to create %s: %s\n", pst->path, strerror(status));
+        } syslog(LOG_INFO, "Created %s\n", pst->path);
+
+        errno = 0;
+        if ( chdir(pst->path) == -1 ) {
+            FAIL("Failed to cd to %s: %s\n", pst->path, strerror(status));
+        }
+    }
+
+    errno = 0;
+    if ( chdir(cwd) == -1 ) {
+        FAIL("Failed to cd %s: %s\n", cwd, strerror(status));
+    }
+
+    // `+ 1` and `- 1` to account for the NUL byte
+    s = snprintf(pst->path + pathlen - 1, 37 + 1, "/%s", pst->hash);
+    if ( s < 0 ) {
+        errno = EXIT_FAILURE;
+        FAIL("Failed to store path path to file\n");
     }
 
     FILE * f = fopen(pst->path, "w+");
