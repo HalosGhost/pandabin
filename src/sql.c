f7c380bfd9134725e47950142f56e9087321ad4b
halosghost <halosghost@archlinux.info>
2017-02-10 19:36:12 -0600

    preliminary insertion

--- src/sql.c
+++ src/sql.c
@@ -35,8 +35,21 @@ pandabin_db_insert (sqlite3 * db, struct pandabin_paste * pst) {
 
     signed status = EXIT_SUCCESS;
 
-    (void )db; (void )pst;
-    goto cleanup;
+    char uuid [37] = "";
+    uuid_unparse_lower(pst->uuid, uuid);
+    status = sqlite3_bind_text(ins_handle, 1, uuid, -1, NULL);
+    status = sqlite3_bind_text(ins_handle, 2, pst->path, -1, NULL);
+    status = sqlite3_bind_int(ins_handle, 3, (signed )pst->size);
+    status = sqlite3_bind_text(ins_handle, 4, pst->hash, -1, NULL);
+
+    if ( status != SQLITE_OK ) {
+        FAIL("Failed to bind arguments: %s\n", sqlite3_errmsg(db));
+    }
+
+    status = sqlite3_step(ins_handle);
+    if ( status != SQLITE_OK ) {
+        FAIL("Failed to execute statement: %s\n", sqlite3_errmsg(db));
+    }
 
     cleanup:
         return status;
