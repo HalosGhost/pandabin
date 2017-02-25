30b687d20eefcdc81022f729e1f3821e280f2306
halosghost <halosghost@archlinux.info>
2017-02-24 19:19:42 -0600

    declare handler for serving pastes

--- src/routes.h
+++ src/routes.h
@@ -8,4 +8,7 @@
 enum lwan_http_status
 serve_index (struct lwan_request *, struct lwan_response *, void *);
 
+enum lwan_http_status
+serve_paste (struct lwan_request *, struct lwan_response *, void *);
+
 #endif
